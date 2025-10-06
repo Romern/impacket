737ffbd3c34c3692c17421054b937c037ca581b6
Romern <github@romern.me>
2025-10-06 04:21:32 +0200

    LDAP relay server: keep relaying

--- impacket/examples/ntlmrelayx/servers/ldaprelayserver.py
+++ impacket/examples/ntlmrelayx/servers/ldaprelayserver.py
@@ -195,8 +195,12 @@ class LDAPHandler(Thread):
 
         self.server.target = self.server.targetprocessor.getTarget()
         if self.server.target is None:
-            logging.info('LDAP: No more targets left!')
-            return
+            if self.server.config.keepRelaying:
+                self.server.config.target.reloadTargets(full_reload=True)
+                self.target = self.server.config.target.getTarget(multiRelay=False)
+            else:
+                logging.info('LDAP: No more targets left!')
+                return
 
         logging.info("LDAP: Relaying credentials for %s to %s://%s" % (username, self.server.target.scheme, self.server.target.netloc))
 
@@ -297,8 +301,12 @@ class LDAPHandler(Thread):
 
         self.server.target = self.server.targetprocessor.getTarget()
         if self.server.target is None:
-            logging.info('LDAP: No more targets left!')
-            return
+            if self.server.config.keepRelaying:
+                self.server.config.target.reloadTargets(full_reload=True)
+                self.server.target = self.server.config.target.getTarget(multiRelay=False)
+            else:
+                logging.info('LDAP: No more targets left!')
+                return
 
         logging.info("LDAP: Relaying credentials to %s://%s" % (self.server.target.scheme, self.server.target.netloc))
 
